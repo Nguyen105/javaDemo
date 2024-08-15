11d2c3d77b6daf5a44d47f7f457b8e9ee8e1e013
Nguyen105 <111992481+Nguyen105@users.noreply.github.com>
2024-08-15 17:20:33 +0100

    Update QuickSortDemo.java

--- QuickSortDemo.java
+++ QuickSortDemo.java
@@ -14,9 +14,9 @@ public class QuickSortDemo extends JPanel {
 	
     public QuickSortDemo(int[] array) {
         this.array = array;
-		this.setLayout(null);    
+	this.setLayout(null);    
         startbutton = new JButton("short");
-		startbutton.setBounds(50,20,150,20);
+	startbutton.setBounds(50,20,150,20);
         startbutton.setFont(new Font("Arial", Font.BOLD, 15));
         startbutton.setFocusPainted(false);    
         startbutton.addActionListener(new ActionListener() {
